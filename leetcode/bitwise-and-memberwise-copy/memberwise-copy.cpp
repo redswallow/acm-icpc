9aeb0217bfaed7c74c11e24f5fcc6beb525aac36
redswallow <redswallowjysc@gmail.com>
2014-01-02 13:19:53 -0800

    replace tab with space

--- leetcode/bitwise-and-memberwise-copy/memberwise-copy.cpp
+++ leetcode/bitwise-and-memberwise-copy/memberwise-copy.cpp
@@ -3,30 +3,29 @@
 
 class A{
 public:
-	A(){
-		m_data = new char(100);
-		printf("default constructor\n");
-	}
-	A(const A& other){
-		m_data = new char(100);
-		memcpy(m_data, other.m_data, strlen(other.m_data));
-		printf("copy constructor\n");
-	}
-	~A(){
-		if(m_data != NULL){
-			delete m_data;
-			m_data=NULL;
-			printf("destructor\n");
-		}
-	}
+    A(){
+        m_data = new char(100);
+        printf("default constructor\n");
+    }
+    A(const A& other){
+        m_data = new char(100);
+        memcpy(m_data, other.m_data, strlen(other.m_data));
+        printf("copy constructor\n");
+    }
+    ~A(){
+        if(m_data != NULL){
+            delete m_data;
+            m_data=NULL;
+            printf("destructor\n");
+        }
+    }
 private:
-	char *m_data;	// pointer
+    char *m_data;    // pointer
 };
 
 int main(int argc, char const *argv[])
 {
-	A a;
-	A b(a); // use memberwise copy
-	return 0;
+    A a;
+    A b(a); // use memberwise copy
+    return 0;
 }
-// bitwise-copy-crash(25789,0x7fff74a78310) malloc: *** error for object 0x7ff628c038f0: pointer being freed was not allocated
--- leetcode/bitwise-and-memberwise-copy/string-copy-constructor.cpp
+++ leetcode/bitwise-and-memberwise-copy/string-copy-constructor.cpp
@@ -2,11 +2,11 @@ class String{
 public:
     String(const String &other);    // copy constructor
 private:
-　　char *m_data;   // store string data
-};  
-　　
-String(const String &other){   
-　　int length = strlen(other.m_data);
-　　m_data = new char[length + 1];
-　　strcpy(m_data, other.m_data);
-} 
+    char *m_data;    // store string data
+};
+
+String(const String &other){
+    int length = strlen(other.m_data);
+    m_data = new char[length + 1];
+    strcpy(m_data, other.m_data);
+}
