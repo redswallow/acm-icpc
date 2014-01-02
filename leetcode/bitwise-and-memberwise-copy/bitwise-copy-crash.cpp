53106ac0dfbc90ff1f5df7454c9f6d3e281d099f
redswallow <redswallowjysc@gmail.com>
2014-01-02 13:06:02 -0800

    bitwise and memberwise copy

---
+++ leetcode/bitwise-and-memberwise-copy/bitwise-copy-crash.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+class A{
+public:
+	A(){
+		m_data = new char(100);
+		printf("default constructor\n");
+	}
+	~A(){
+		if(m_data != NULL){
+			delete m_data;
+			m_data=NULL;
+			printf("destructor\n");
+		}
+	}
+private:
+	char *m_data;	// pointer
+};
+
+int main(int argc, char const *argv[])
+{
+	A a;
+	A b(a); // default: use bitwise copy
+	return 0;
+}
+// bitwise-copy-crash(25789,0x7fff74a78310) malloc: *** error for object 0x7ff628c038f0: pointer being freed was not allocated
