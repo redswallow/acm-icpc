223eba81459a47e8de86ab831172c0b5acf809a5
redswallow <redswallowjysc@gmail.com>
2014-01-04 23:47:33 -0800

    Next Permutation solution

---
+++ leetcode/next-permutation.cpp
@@ -0,0 +1,12 @@
+void nextPermutation(vector<int> &num){
+    for (int i=num.size()-2;i>=0;--i)
+        if (num[i]<num[i+1])
+            for (int j=num.size()-1;j>=i;--j)
+                if (num[j]>num[i]){
+                    swap(num[i],num[j]);
+                    reverse(num.begin()+i+1,num.end());
+                    return;
+                }
+    reverse(num.begin(),num.end());
+    return;
+}
